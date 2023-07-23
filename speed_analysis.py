4a763afe98a07fb8dbb43e386ee66a9a6c8d9655
Everloom-129 <jie.20@intl.zju.edu.cn>
2023-07-23 10:45:39 +0800

    module seperation

--- speed_analysis.py
+++ speed_analysis.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
-from imutils.object_detection import non_max_suppression
-import imutils
+# other library you need 
 
 # Step 1: Video Frame Extraction
 def extract_frames(video_path):
